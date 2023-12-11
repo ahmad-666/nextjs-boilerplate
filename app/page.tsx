7115c39445d266d0aa381f93fa3431eb1205c5f2
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-12-11 22:19:32 +0330

    chore: add ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import SectionContainer from "@/app/styled-components/SectionContainer";
 import TestComp from "@/app/components/TestComp";
 
+//we can use bellow params on layout,pages,route-handlers
+export const dynamic = "force-dynamic"; //'auto' | 'force-dynamic'(SSR) | 'error' | 'force-static'(SSG)
+//export const revalidate = false //false | 'force-cache' | 0 | number(in seconds,ISR)
 export default async function Page({}) {
   return (
     <div>
